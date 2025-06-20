67848a77abd9e3c409c570cd0fb15c2b5b71d0b9
Fares <126884184+Faresabdelghany@users.noreply.github.com>
2025-06-20 08:00:19 +0300

    Add Jest configuration for TypeScript testing

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.test.ts',
+    '**/?(*.)+(spec|test).ts'
+  ],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/__tests__/**',
+    '!src/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@/infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
+    '^@/tools/(.*)$': '<rootDir>/src/tools/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  testTimeout: 30000,
+};
